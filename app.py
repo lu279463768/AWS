688bb00aea3cc3c8a915898a3f99652940db59a2
vivian <lu279463768@gmail.com>
2023-12-25 14:27:39 -0500

    some edits

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template_string
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template_string("""
+    <html>
+        <head>
+            <title>My Simple Website</title>
+        </head>
+        <body>
+            <h1>Welcome to my simple website!</h1>
+        </body>
+    </html>
+    """)
+
+if __name__ == '__main__':
+    app.run(debug=True)
